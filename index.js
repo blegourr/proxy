548471aee6b73db86e5e9da5242014296424f450
Blegourr <91756178+blegourr@users.noreply.github.com>
2024-01-28 18:52:24 +0100

    test correction of 2 bugs

--- index.js
+++ index.js
@@ -1,31 +1,23 @@
-/*--------------------------------------------------------------------
- *                         INITIALISATION
- *--------------------------------------------------------------------
-**/
 const Koa = require('koa');
 const https = require('https');
+const httpProxy = require('http-proxy');
 const { default: enforceHttps } = require('koa-sslify');
 const path = require('path');
 const fs = require('fs');
 const dotenv = require('dotenv');
 dotenv.config();
-const { createProxyMiddleware } = require('http-proxy-middleware');
 
-
-/*--------------------------------------------------------------------
- *                       CREATION SERVEUR
- *--------------------------------------------------------------------
-**/
-// init
 const app = new Koa();
-
 
 let options = {
   key: fs.readFileSync(path.join(__dirname, './ssl/private.pem')),
   cert: fs.readFileSync(path.join(__dirname, './ssl/public.pem'))
-}
+};
 
-// Middleware for parsing request body
+// Create a proxy instance
+const proxy = httpProxy.createProxyServer();
+
+// Middleware for proxying requests
 app.use(async (ctx, next) => {
   // Récupérer le sous-domaine de la requête
   const subdomain = ctx.hostname.split('.')[0];
@@ -33,23 +25,19 @@ app.use(async (ctx, next) => {
   // Rediriger les requêtes en fonction du sous-domaine
   if (subdomain === 'organim') {
     // Rediriger les requêtes vers le serveur x sur le port spécifié
-    console.log(process.env.REDIRECTION_ORGANIMPROD);
-    return createProxyMiddleware({
-      target: `${process.env.REDIRECTION_ORGANIMPROD}`,
-      changeOrigin: false,
-    })(ctx, next);
+    proxy.web(ctx.req, ctx.res, {
+      target: process.env.REDIRECTION_ORGANIMPROD
+    });
   } else if (subdomain === 'organimDev') {
     // Rediriger les requêtes vers le serveur y sur le port spécifié
-    return createProxyMiddleware({
-      target: `${process.env.REDIRECTION_ORGANIMDEV}`,
-      changeOrigin: false,
-    })(ctx, next);
+    proxy.web(ctx.req, ctx.res, {
+      target: process.env.REDIRECTION_ORGANIMDEV
+    });
   } else {
     // Rediriger les requêtes vers le serveur y sur le port spécifié
-    return createProxyMiddleware({
-      target: `${process.env.REDIRECTION_ORGANIMPROD}`,
-      changeOrigin: false,
-    })(ctx, next);
+    proxy.web(ctx.req, ctx.res, {
+      target: process.env.REDIRECTION_ORGANIMPROD
+    });
   }
   // Ajoutez plus de conditions pour d'autres sous-domaines si nécessaire
 
@@ -57,11 +45,7 @@ app.use(async (ctx, next) => {
   await next();
 });
 
-/*----------------------------------------------------
- *               Configuration Serveur
- *----------------------------------------------------
-**/
-// Démarrez le serveur sur le port process.env.PORT || 443;
+// Create HTTPS server
 const PORT = process.env.PORT || 49152;
 
 app.use(enforceHttps({
