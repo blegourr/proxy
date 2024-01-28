59eef32dd26458a1f41f44b6e72944c4c16c95cc
Blegourr <91756178+blegourr@users.noreply.github.com>
2024-01-28 14:19:04 +0100

    oki test

---
+++ ecosystem.production.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  apps : [{
+      name   : "proxy-production",
+      script: './index.js',
+      watch: '.',
+      env_production: {
+          "PORT": 443
+      }
+  }],
+
+  deploy : {
+      production : {
+          user : 'blegourr',
+          host : '192.168.1.1',
+          key  : './gitub_action_key',
+          ref  : 'origin/master',
+          repo : 'git@github.com:blegourr/proxy.git',
+          path : '/var/www/blegourr_production',
+          'post-setup': 'npm install',
+          'post-deploy' : 'pm2 reload ecosystem.production.config.js --env production',
+      }
+  }
+};
